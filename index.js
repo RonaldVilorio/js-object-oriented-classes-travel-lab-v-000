a5ea1d6913a5e862eb0b1159f950545a8ae5cfd2
ronald vilorio <soccer23415@gmail.com>
2018-05-17 20:26:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ class Route{
     this.endingLocation = endingLocation
   }
   blocksTravelled(){
-    debugger
+    // debugger
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
     this.beginningLocation["vertical"] - this.endingLocation["vertical"]
 
