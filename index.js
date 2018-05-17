6ea4af89d8f741bf7b6ce1bf3aa6bc273c49bf09
ronald vilorio <soccer23415@gmail.com>
2018-05-17 22:14:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ class Route{
     const today = new Date
     // return today.getHours() >= 22 && today.getHours() <= 24 ? this.blocksTravelled() / 2 : this.blocksTravelled() / 3
     // (today.getHours() >= 5 && today.getHours() <= 10)
-    if (this.blocksTravelled > 46) {
+    if (this.blocksTravelled) {
       return this.blocksTravelled() / 2
     }else {
       return this.blocksTravelled() / 3
