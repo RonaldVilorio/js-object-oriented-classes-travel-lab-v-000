0abf0ef55cc167a6af21e6077de1fcd4142818c5
ronald vilorio <soccer23415@gmail.com>
2018-05-17 21:38:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ class Route{
   estimatedTime(){
     // debugger
     // return Date.prototype.getHours
-    today = new Date
+    const today = new Date
     if (today.getHours() >= 22 && today.getHours() <= 24){
       this.blocksTravelled() / 2
     }else {
