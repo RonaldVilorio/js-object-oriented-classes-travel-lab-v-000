8878bd26622114b89fc20247fd07a391446c5237
ronald vilorio <soccer23415@gmail.com>
2018-05-17 20:10:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(endDate){
+    return endDate - this.startDate.getFullYear()
+  }
+}
+class Route{
+  constructor(horizontal,vertical){
+    this.horizontal = horizontal
+    this.vertical = vertical
+  }
+  blocksTravelled(){
+    // debugger
+    if (this.vertical["vertical"] > this.horizontal["vertical"]){
+      return this.vertical["vertical"] - this.horizontal["vertical"]
+    }else if(this.vertical["vertical"] === this.horizontal["vertical"]){
+      return 4
+    }
+  }
 
+}
